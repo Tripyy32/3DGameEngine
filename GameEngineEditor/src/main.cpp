2725ff0879e5787c8e539239d7bec931550952cc
Daniil <daddadov545@gmail.com>
2024-11-22 22:20:31 +0500

    added base architecture of game engine and linked a glfw library

---
+++ GameEngineEditor/src/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<GameEngineCore/Utils/test.hpp>
+
+int main() {
+	std::cout << "Hello from GameEngineEditor!" << std::endl;
+
+	GameEngine::checkGLFW();
+
+	std::cin.get();
+
+}
